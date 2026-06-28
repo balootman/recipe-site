a3be8b01d674df203a885b50915e1742034689f8
balootman <169062149+balootman@users.noreply.github.com>
2026-06-28 19:49:17 +0300

    Connect shared Supabase recipe database

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.RECIPE_APP_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://kxlaftyqvlbpwiarmatn.supabase.co",
+  supabaseAnonKey: "sb_publishable_144w4thmz5sUT-JxV9FHOA_FJHSMaQP"
 };
